dc04e2ed3633d2c8ad7a55e30d68276c8a953c5f
kaeuhy <20212105@edu.hanbat.ac.kr>
2025-08-11 18:29:39 +0900

    공통 설정과 API 요청 함수 정의

--- app.js
+++ app.js
@@ -21,3 +21,44 @@ app.use(
         cookie: {secure: false},
     })
 );
+
+// 변수 지정
+const client_id = "0918856d58725160c41708e592be8475";
+const client_secret = "this is client secret key";
+const domain = "http://localhost:4000";
+const redirect_uri = `${domain}/redirect`;
+const token_uri = "https://kauth.kakao.com/oauth/token"; // 액세스 토큰 요청을 보낼 카카오 인증 서버 주소
+const api_host = "https://kapi.kakao.com";
+
+// API 요청 함수 정의
+async function call(method, uri, param, header) {
+    let rtn;
+    try {
+        // 지정된 method, uri, param, header 값을 사용해 카카오 API 서버로 HTTP 요청 전송
+        rtn = await axios({
+            method: method,   // POST 또는 GET 등 HTTP 메서드
+            url: uri,         // 요청할 API 주소
+            headers: header,  // 요청 헤더
+            data: param,      // 전송할 요청 데이터
+        });
+    } catch (err) {
+        // 오류 발생 시, 응답 객체에서 오류 응답 내용 저장
+        rtn = err.response;
+    }
+    // 요청 성공 또는 실패에 상관없이 응답 데이터 반환
+    return rtn.data;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
